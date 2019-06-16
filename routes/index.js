3b19453851f6b9afc1453a42172aa6afc5b3fbbf
Mikkubhardwaj <51876234+Mikkubhardwaj@users.noreply.github.com>
2019-06-16 15:37:57 +0530

    version 1
    without node_module.

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('Home');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('About');
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('Contact');
+});
+
+
+module.exports = router;
